2409c8ca375e2b1315f899124f6a50355057a1e6
joyumoekpo <joyumoekpo1@gmail.com>
2023-01-15 18:39:32 -0600

    Changed date values

--- expense-tracker/src/components/Expenses/ExpensesFilter.js
+++ expense-tracker/src/components/Expenses/ExpensesFilter.js
@@ -12,14 +12,14 @@ const ExpensesFilter = (props) => {
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
         <select value={props.selected} onChange={dropdownChangeHandler}>
-          <option value='2022'>2030</option>
-          <option value='2021'>2029</option>
-          <option value='2020'>2028</option>
-          <option value='2019'>2027</option>
-          <option value='2022'>2026</option>
-          <option value='2021'>2025</option>
-          <option value='2020'>2024</option>
-          <option value='2019'>2023</option>
+          <option value='2030'>2030</option>
+          <option value='2029'>2029</option>
+          <option value='2028'>2028</option>
+          <option value='2027'>2027</option>
+          <option value='2026'>2026</option>
+          <option value='2025'>2025</option>
+          <option value='2024'>2024</option>
+          <option value='2023'>2023</option>
         </select>
       </div>
     </div>
